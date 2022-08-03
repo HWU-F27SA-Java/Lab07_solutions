be24f2ea6e890f746643f01a7345e6dee59eac64
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 15:00:55 +0100

    Create Air.java

---
+++ src/exercise6/Air.java
@@ -0,0 +1,29 @@
+package exercise6;
+import java.util.Comparator;
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hadj
+ */
+public class Air extends Shipment{
+	private final int costFactor = 10;
+	private final int DISTANCE_FACTOR = 1000;
+
+	public Air(Comparator<? super Merchandise> cmp) {
+        super(cmp);
+        
+    }
+
+
+    @Override
+    public double calculateMerchandiseCost(Merchandise m) {
+        return costFactor  * m.getWeight() * (distance / DISTANCE_FACTOR);
+    }
+    
+}
