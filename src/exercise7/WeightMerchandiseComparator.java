d32498010651c0f3e2d80767ee1bb37de302a98b
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 15:01:44 +0100

    Create WeightMerchandiseComparator.java

---
+++ src/exercise7/WeightMerchandiseComparator.java
@@ -0,0 +1,21 @@
+package exercise7;
+
+import java.util.Comparator;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hadj
+ */
+public class WeightMerchandiseComparator implements Comparator<Merchandise>{
+
+    @Override
+    public int compare(Merchandise t1, Merchandise t2) {
+        return t1.getWeight().compareTo(t2.getWeight());
+    }
+    
+}
